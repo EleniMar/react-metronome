33f83d9bde50a48e7f092a13cd7918beaf3a0a07
EleniMar <elenimpea@gmail.com>
2020-09-13 05:33:19 +0300

    Updates

---
+++ precache-manifest.7696d0ca1a92814db3b7a4f9c07e367d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8262c503aa038c14a1371d298087470b",
+    "url": "/react-metronome/index.html"
+  },
+  {
+    "revision": "8f1020c6513ef40b3ee6",
+    "url": "/react-metronome/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "b9690d104621a5763c38",
+    "url": "/react-metronome/static/css/main.998ba8a7.chunk.css"
+  },
+  {
+    "revision": "8f1020c6513ef40b3ee6",
+    "url": "/react-metronome/static/js/2.8d3f1cea.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-metronome/static/js/2.8d3f1cea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9690d104621a5763c38",
+    "url": "/react-metronome/static/js/main.d51b7c82.chunk.js"
+  },
+  {
+    "revision": "44d01263d4ca52b8ffc6",
+    "url": "/react-metronome/static/js/runtime-main.693ebd43.js"
+  },
+  {
+    "revision": "7a7930a26ffe78ed1c7375364bde24c2",
+    "url": "/react-metronome/static/media/click1.7a7930a2.wav"
+  },
+  {
+    "revision": "fe0ded65f5f6b999883ee7aa4cf418c4",
+    "url": "/react-metronome/static/media/click2.fe0ded65.wav"
+  }
+]);
